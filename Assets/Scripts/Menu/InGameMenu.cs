4faa1dafcf1b23ef42b7c8a25fff6a08d1c9fb0e
Luca-Caporaloni <lucacaporaloni@gmail.com>
2024-06-23 17:22:54 -0300

    Shaders y juego ya listo :dog:

--- Assets/Scripts/Menu/InGameMenu.cs
+++ Assets/Scripts/Menu/InGameMenu.cs
@@ -10,10 +10,21 @@ public class InGameMenu : MonoBehaviour
     public TMP_Text objectivesText; // Referencia al TextMeshPro para los objetivos
     private bool isMenuActive = false;
 
+    public FirstPersonMovement movementScript; // Referencia al script de movimiento del jugador
+    public FirstPersonLook lookScript; // Referencia al script de mirada del jugador
+    public Camera playerCamera;
+
+    public Rigidbody playerRigidbody;
+
+    private RigidbodyConstraints originalConstraints;
+
     void Start()
     {
         menuCanvas.SetActive(false); // Asegúrate de que el menú esté desactivado al inicio
         UpdateInstructions(); // Actualizar las instrucciones al inicio
+
+        playerCamera = Camera.main;
+        originalConstraints = playerRigidbody.constraints;
     }
 
     void Update()
@@ -30,6 +41,19 @@ public class InGameMenu : MonoBehaviour
         menuCanvas.SetActive(isMenuActive);
         Cursor.lockState = isMenuActive ? CursorLockMode.None : CursorLockMode.Locked; // Liberar o bloquear el cursor
         Cursor.visible = isMenuActive; // Hacer visible o invisible el cursor
+
+        if (isMenuActive)
+        {
+            movementScript.enabled = false; // Desactivar el movimiento del jugador
+            lookScript.enabled = false; // Desactivar la rotación de la cámara del jugador
+            playerRigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        }
+        else
+        {
+            movementScript.enabled = true; // Activar el movimiento del jugador
+            lookScript.enabled = true; // Activar la rotación de la cámara del jugador
+            playerRigidbody.constraints = originalConstraints; // Restaurar las restricciones originales del jugador
+        }
     }
 
     public void UpdateInstructions()
@@ -62,5 +86,8 @@ public class InGameMenu : MonoBehaviour
         menuCanvas.SetActive(false);
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        movementScript.enabled = true; // Reactivar el movimiento del jugador
+        lookScript.enabled = true; // Reactivar la rotación de la cámara del jugador
+        playerRigidbody.constraints = originalConstraints; // Restaurar las restricciones originales del jugador
     }
 }
